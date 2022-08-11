90a0843aef3c2f0b86eab52546d2fdbe7c0c8d1e
flyingskyhigh <elitejoey9@gmail.com>
2022-08-11 16:19:58 -0700

    Initialized home page

--- apps/nft-marketplace/src/pages/Home.tsx
+++ apps/nft-marketplace/src/pages/Home.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import back from '../assets/images/back.png';
 
 const Home = () => {
-  return (
-    <div className="home-back">
-      <div className="flex justify-center items-center">
-        <div className="text-white font-drone">Hello world</div>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default Home;
