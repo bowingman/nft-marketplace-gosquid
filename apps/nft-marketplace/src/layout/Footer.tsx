209ab777300660558e2bdc3e5b0f530cf8215cfe
Dennis <Dennis@gmail.com>
2022-08-11 20:36:11 -0700

    added: footer

--- apps/nft-marketplace/src/layout/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <h1 className="text-white">footer</h1>;
-};
-
-export default Footer;
--- apps/nft-marketplace/src/pages/Home.tsx
+++ apps/nft-marketplace/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import back from '../assets/images/back.png';
+import Footer from '../layout/footer';
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
