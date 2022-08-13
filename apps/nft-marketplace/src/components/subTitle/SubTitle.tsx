f5487ff2d2163a6c28f93fe134c2d7fe5352e072
flyingskyhigh <elitejoey9@gmail.com>
2022-08-12 19:59:37 -0700

    Added subtitle

--- apps/nft-marketplace/src/components/subTitle/SubTitle.tsx
+++ apps/nft-marketplace/src/components/subTitle/SubTitle.tsx
@@ -1,20 +1,24 @@
+import ReactSm from '../../assets/images/rect-sm.svg';
 export type SubTitleProps = {
   title: string;
   description?: string;
 };
 
 const SubTitle = ({ title, description }: SubTitleProps) => {
-  console.log(!!description);
   return (
-    <div className="flex flex-col items-center w-1/2">
-      <h3 className="title-40-text uppercase text-yellow">{title}</h3>
+    <div className="mt-[120px] w-full md:w-1/2">
+      <h1 className="title-40-text text-center text-yellow">{title}</h1>
       {!!description ? (
         <p className="header-16-medium-text text-center">{description}</p>
       ) : (
         ''
       )}
-      <div className="w-[240px] h-[2px] bg-white mt-[24px]"></div>
-      <div className="w-[50px] h-1 bg-white"></div>
+      <div className="flex justify-center mt-[24px]">
+        <div className="w-[240px] h-[2px] bg-white"></div>
+      </div>
+      <div className="flex justify-center">
+        <img src={ReactSm} alt="" />
+      </div>
     </div>
   );
 };
