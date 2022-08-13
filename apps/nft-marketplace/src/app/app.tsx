e7e1ef3f743fc8b5834f38e640cb2ab3c94e3104
flyingskyhigh <elitejoey9@gmail.com>
2022-08-12 19:58:18 -0700

    Added nav

--- apps/nft-marketplace/src/app/app.tsx
+++ apps/nft-marketplace/src/app/app.tsx
@@ -1,9 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Home from '../pages/home';
+import Nav from '../layout/nav';
 
 export function App() {
   return (
     <>
+      <Nav />
       <Home />
     </>
   );
