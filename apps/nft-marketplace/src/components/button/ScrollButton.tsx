7834659e04d9a937b7604efaea2bb45ab65b28fb
rabbit <rabbit@sky.io>
2022-08-13 18:57:33 -0700

    added: scroll

--- apps/nft-marketplace/src/components/button/ScrollButton.tsx
+++ apps/nft-marketplace/src/components/button/ScrollButton.tsx
@@ -6,7 +6,16 @@ const ScrollButton = () => {
     });
   };
 
-  return <button onClick={scrollToTop}>Scroll down</button>;
+  return (
+    <div className="absolute left-[50%] bottom-[60px]">
+      <button onClick={scrollToTop} className="font-bold text-base font-inter">
+        Scroll down
+      </button>
+      <div className="flex justify-center">
+        <img src="/assets/images/vector.png" alt="vector.png"></img>
+      </div>
+    </div>
+  );
 };
 
 export default ScrollButton;
